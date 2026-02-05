b672b457e2b10f53975fd8d6cf85d36517aa60a8
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-02-05 23:42:53 +0900

    refactor(baseball): main 메서드 로직 기능별 메서드로 분리

--- 01-baseball/src/main/java/baseball/Application.java
+++ 01-baseball/src/main/java/baseball/Application.java
@@ -14,76 +14,17 @@ public class Application {
         boolean isGameRunning = true;
         while (isGameRunning) {
 
-            List<Integer> computer = new ArrayList<>();
-            while (computer.size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if (!computer.contains(randomNumber)) {
-                    computer.add(randomNumber);
-                }
-            }
+            List<Integer> computer = generateComputerNumbers();
 
             while (true) {
 
                 System.out.print("숫자를 입력해주세요 : ");
-
                 String input = Console.readLine();
+                validateInput(input);
 
-                if (input.length() != 3) {
-                    throw new IllegalArgumentException();
-                }
+                List<Integer> user = parseUserNumbers(input);
 
-                for (int i = 0; i < input.length(); i++) {
-
-                    char c = input.charAt(i);
-
-                    if (!Character.isDigit(c)) {
-                        throw new IllegalArgumentException();
-                    }
-
-                    if (c == '0') {
-                        throw new IllegalArgumentException();
-                    }
-
-                }
-
-                if (input.charAt(0) == input.charAt(1) ||
-                        input.charAt(1) == input.charAt(2) ||
-                        input.charAt(0) == input.charAt(2)) {
-                    throw new IllegalArgumentException();
-                }
-
-                List<Integer> user = new ArrayList<>();
-                for (String s : input.split("")) {
-                    user.add(Integer.parseInt(s));
-                }
-
-                int strike = 0;
-                int ball = 0;
-
-                for (int i = 0; i < user.size(); i++) {
-
-                    if (user.get(i).equals(computer.get(i))) {
-                        strike++;
-                    } else if (computer.contains(user.get(i))) {
-                        ball++;
-                    }
-                }
-
-                if (ball != 0 && strike != 0) {
-                    System.out.println(ball + "볼 " + strike + "스트라이크");
-                }
-
-                else if (ball != 0) {
-                    System.out.println(ball + "볼");
-                }
-
-                else if (strike != 0) {
-                    System.out.println(strike + "스트라이크");
-                }
-
-                else {
-                    System.out.println("낫싱");
-                }
+                int strike = calculateScore(computer, user);
 
                 if (strike == 3) {
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
@@ -101,5 +42,86 @@ public class Application {
                 }
             }
         }
+    }
+
+    private static void validateInput(String input) {
+
+        if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+
+            char c = input.charAt(i);
+
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException();
+            }
+
+            if (c == '0') {
+                throw new IllegalArgumentException();
+            }
+
+        }
+
+        if (input.charAt(0) == input.charAt(1) ||
+                input.charAt(1) == input.charAt(2) ||
+                input.charAt(0) == input.charAt(2)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static List<Integer> generateComputerNumbers() {
+
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
+
+    private static List<Integer> parseUserNumbers(String input) {
+
+        List<Integer> user = new ArrayList<>();
+
+        for (String s : input.split("")) {
+            user.add(Integer.parseInt(s));
+        }
+
+        return user;
+    }
+
+    private static int calculateScore(List<Integer> computer, List<Integer> user) {
+
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < user.size(); i++) {
+
+            if (user.get(i).equals(computer.get(i))) {
+                strike++;
+            } else if (computer.contains(user.get(i))) {
+                ball++;
+            }
+        }
+
+        if (ball != 0 && strike != 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        } else if (ball != 0) {
+            System.out.println(ball + "볼");
+        } else if (strike != 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println("낫싱");
+        }
+
+        return strike;
     }
 }
