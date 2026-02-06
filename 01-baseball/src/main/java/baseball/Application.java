e0f4f4c876dbf2434584f9b01f7eba83add85cb9
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-02-07 00:24:07 +0900

    refactor(baseball): 매직 넘버 및 출력 메시지 상수화 처리

--- 01-baseball/src/main/java/baseball/Application.java
+++ 01-baseball/src/main/java/baseball/Application.java
@@ -7,9 +7,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+
+
+    private static final int NUMBER_LENGTH = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+
+    private static final String RESTART_COMMAND = "1";
+    private static final String EXIT_COMMAND = "2";
+
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_OVER_MESSAGE = NUMBER_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 " + RESTART_COMMAND + ", 종료하려면 " + EXIT_COMMAND + "를 입력하세요.";
+
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String BALL_MESSAGE = "볼";
+    private static final String NOTHING_MESSAGE = "낫싱";
+
     public static void main(String[] args) throws IllegalArgumentException {
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(GAME_START_MESSAGE);
 
         boolean isGameRunning = true;
         while (isGameRunning) {
@@ -18,7 +36,7 @@ public class Application {
 
             while (true) {
 
-                System.out.print("숫자를 입력해주세요 : ");
+                System.out.print(INPUT_MESSAGE);
                 String input = Console.readLine();
                 validateInput(input);
 
@@ -26,14 +44,14 @@ public class Application {
 
                 int strike = calculateScore(computer, user);
 
-                if (strike == 3) {
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                if (strike == NUMBER_LENGTH) {
+                    System.out.println(GAME_OVER_MESSAGE);
+                    System.out.println(GAME_RESTART_MESSAGE);
 
                     String command = Console.readLine();
-                    if (command.equals("1")) {
+                    if (command.equals(RESTART_COMMAND)) {
                         break;
-                    } else if (command.equals("2")) {
+                    } else if (command.equals(EXIT_COMMAND)) {
                         isGameRunning = false;
                         break;
                     } else {
@@ -46,7 +64,7 @@ public class Application {
 
     private static void validateInput(String input) {
 
-        if (input.length() != 3) {
+        if (input.length() != NUMBER_LENGTH) {
             throw new IllegalArgumentException();
         }
 
@@ -75,9 +93,9 @@ public class Application {
 
         List<Integer> computer = new ArrayList<>();
 
-        while (computer.size() < 3) {
+        while (computer.size() < NUMBER_LENGTH) {
 
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
 
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
@@ -113,13 +131,13 @@ public class Application {
         }
 
         if (ball != 0 && strike != 0) {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
+            System.out.println(ball + BALL_MESSAGE + " " + strike + STRIKE_MESSAGE);
         } else if (ball != 0) {
-            System.out.println(ball + "볼");
+            System.out.println(ball + BALL_MESSAGE);
         } else if (strike != 0) {
-            System.out.println(strike + "스트라이크");
+            System.out.println(strike + STRIKE_MESSAGE);
         } else {
-            System.out.println("낫싱");
+            System.out.println(NOTHING_MESSAGE);
         }
 
         return strike;
